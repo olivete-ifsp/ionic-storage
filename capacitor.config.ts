1d5d2644abb98a7e88548e3dc35703121131ba2f
Andre Luis Olivete <olivete@ifsp.edu.br>
2023-11-29 22:29:45 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trabalhando_storage',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
